0501c5c64a179649138033bfd84fa4fca06c370c
Lukas Waymann <meribold@gmail.com>
2018-09-21 19:15:07 +0800

    Save photos in a directory owned by my app
    Also don't show a thumbnail for ants in the `ImageView`; show the actual
    photo.

--- app/src/main/java/MainActivity.kt
+++ app/src/main/java/MainActivity.kt
@@ -6,11 +6,17 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
+import java.io.File
+import java.io.IOException
+import android.support.v4.content.FileProvider
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private var photo: Bitmap? = null
     private var photoRequestMade = false
+    private var photoUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,9 +35,25 @@ class MainActivity : AppCompatActivity() {
     // See <https://developer.android.com/training/camera/photobasics>.
     private fun snap() {
         val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        if (takePictureIntent.resolveActivity(packageManager) != null) {
-            startActivityForResult(takePictureIntent, 1)
+        if (takePictureIntent.resolveActivity(packageManager) == null) {
+            TODO()
         }
+        var photoFile: File?
+        try {
+            photoFile = File.createTempFile("photo_", ".jpg", getExternalFilesDir(null))
+            // currentPhotoPath = photoFile.absolutePath
+        } catch (e: IOException) {
+            TODO()
+        }
+        // See <https://stackoverflow.com/a/44212615>.
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT) {
+            photoUri = Uri.fromFile(photoFile)
+        } else {
+            photoUri = FileProvider.getUriForFile(this,
+                "xyz.meribold.snapscore.fileprovider", photoFile)
+        }
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+        startActivityForResult(takePictureIntent, 1)
         photoRequestMade = true
     }
 
@@ -40,10 +62,9 @@ class MainActivity : AppCompatActivity() {
         if (resultCode != Activity.RESULT_OK) {
             finish()
         }
-        photo = data?.extras?.get("data") as Bitmap?
-        if (photo == null) {
+        if (photoUri == null) {
             finish()
         }
-        imageView.setImageBitmap(photo)
+        imageView.setImageURI(photoUri)
     }
 }
