8b7c43a78faad6c038515d75d30a8107b00e5a8d
Lukas Waymann <meribold@gmail.com>
2018-09-21 12:55:13 +0800

    Fix missing space

--- app/src/main/java/MainActivity.kt
+++ app/src/main/java/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (photo == null){
+        if (photo == null) {
             snap()
         }
     }
