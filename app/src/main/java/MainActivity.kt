293cf0d530a19d42a9dbed3f2b8336388d265fa1
Lukas Waymann <meribold@gmail.com>
2018-09-22 18:42:20 +0800

    Fix showing photos with the wrong orientation
    On some phones, for example my Sony Xperia T3 D5103, photos are shown
    with the wrong orientation.  This hopefully resolves that issue.  See
    [1] and [2].
    
    [1]: https://stackoverflow.com/q/14066038
    [2]: https://youtu.be/mZbL2ofcdP0

--- app/src/main/java/MainActivity.kt
+++ app/src/main/java/MainActivity.kt
@@ -2,6 +2,10 @@ package xyz.meribold.snapscore
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.media.ExifInterface
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -64,6 +68,25 @@ class MainActivity : AppCompatActivity() {
         if (photoUri == null) {
             finish()
         }
-        imageView.setImageURI(photoUri)
+        val bitmap = BitmapFactory.decodeFile(photoUri!!.getPath())
+        imageView.setImageBitmap(fixBitmapOrientation(bitmap))
+    }
+
+    private fun fixBitmapOrientation(bitmap: Bitmap): Bitmap {
+        val exif = ExifInterface(photoUri!!.path)
+        val orientation: Int = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                                                    ExifInterface.ORIENTATION_NORMAL)
+        return when (orientation) {
+            ExifInterface.ORIENTATION_ROTATE_90  -> rotateBitmap(bitmap, 90f)
+            ExifInterface.ORIENTATION_ROTATE_180 -> rotateBitmap(bitmap, 180f)
+            ExifInterface.ORIENTATION_ROTATE_270 -> rotateBitmap(bitmap, 270f)
+                                            else -> bitmap
+        }
+    }
+
+    private fun rotateBitmap(bitmap: Bitmap, degrees: Float): Bitmap {
+        val matrix = Matrix()
+        matrix.postRotate(degrees)
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
     }
 }
