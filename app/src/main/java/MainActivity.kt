23ee86c4dc2a3643f505e8bd5540d02e40f4fe82
Lukas Waymann <meribold@gmail.com>
2018-09-23 22:38:11 +0800

    Load photos efficiently

--- app/src/main/java/MainActivity.kt
+++ app/src/main/java/MainActivity.kt
@@ -12,6 +12,7 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.support.v4.content.FileProvider
 import android.support.v7.app.AppCompatActivity
+import android.util.DisplayMetrics
 import android.widget.Toast
 import java.io.File
 import java.io.IOException
@@ -83,15 +84,44 @@ class MainActivity : AppCompatActivity() {
     }
     // [1]: https://developer.android.com/training/camera/photobasics
 
+    // This shows the bitmap without wasting too much memory.  See [1].
     private fun showBitmap(path: String) {
-        val bitmap: Bitmap? = BitmapFactory.decodeFile(path)
+        // Get the display size.  What I really would like to have is the size the
+        // `ImageView` *could* have in the current layout (i.e., the size it would have
+        // when showing an arbitrarily big image), but I don't know how to get that.
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        val availableWidth  = displayMetrics.widthPixels
+        val availableHeight = displayMetrics.heightPixels
+
+        val options = BitmapFactory.Options()
+        options.inJustDecodeBounds = true
+        BitmapFactory.decodeFile(path, options)
+        val orientation: Int = getImageOrientation(path)
+        val swapWidthAndHeight = orientation == ExifInterface.ORIENTATION_ROTATE_90 ||
+                                 orientation == ExifInterface.ORIENTATION_ROTATE_270
+        options.inSampleSize = if (swapWidthAndHeight) {
+            calculateInSampleSize(options.outHeight, options.outWidth,
+                                  availableWidth, availableHeight)
+        } else {
+            calculateInSampleSize(options.outWidth, options.outHeight,
+                                  availableWidth, availableHeight)
+        }
+        options.inJustDecodeBounds = false
+        val bitmap: Bitmap? = BitmapFactory.decodeFile(path, options)
         if (bitmap == null) {
             Toast.makeText(getApplicationContext(), "Decoding bitmap failed.",
                            Toast.LENGTH_LONG).show()
         } else {
-            imageView.setImageBitmap(fixBitmapOrientation(bitmap, path))
+            imageView.setImageBitmap(when (orientation) {
+                ExifInterface.ORIENTATION_ROTATE_90  -> rotateBitmap(bitmap, 90f)
+                ExifInterface.ORIENTATION_ROTATE_180 -> rotateBitmap(bitmap, 180f)
+                ExifInterface.ORIENTATION_ROTATE_270 -> rotateBitmap(bitmap, 270f)
+                                                else -> bitmap
+            })
         }
     }
+    // [1]: https://developer.android.com/topic/performance/graphics/load-bitmap
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         assert(requestCode == 1)
@@ -105,20 +135,33 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-private fun fixBitmapOrientation(bitmap: Bitmap, path: String): Bitmap {
-    val exif = ExifInterface(path)
-    val orientation: Int = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION,
-                                                ExifInterface.ORIENTATION_NORMAL)
-    return when (orientation) {
-        ExifInterface.ORIENTATION_ROTATE_90  -> rotateBitmap(bitmap, 90f)
-        ExifInterface.ORIENTATION_ROTATE_180 -> rotateBitmap(bitmap, 180f)
-        ExifInterface.ORIENTATION_ROTATE_270 -> rotateBitmap(bitmap, 270f)
-                                        else -> bitmap
-    }
-}
+private fun getImageOrientation(path: String): Int =
+    ExifInterface(path).getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                                        ExifInterface.ORIENTATION_NORMAL)
 
 private fun rotateBitmap(bitmap: Bitmap, degrees: Float): Bitmap {
     val matrix = Matrix()
     matrix.postRotate(degrees)
     return Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
 }
+
+// This is similar to the function with the same name at [1], but returns a value for more
+// aggressive subsampling.  The function at [1] still resulted in bitmaps that failed to
+// display on a Galaxy S4 GT-I9500 because of being too big (apparently that phone doesn't
+// like bitmaps with more than 4096 pixels along one dimension).
+fun calculateInSampleSize(width: Int, height: Int, availableWidth: Int,
+                          availableHeight: Int): Int {
+    var inSampleSize = 1
+
+    // Calculate the largest `inSampleSize` value that is a power of 2 and keeps the
+    // height or width larger than 75% of the available height or width, respectively.  We
+    // only require one dimension to stay larger than (75% of) its respective available
+    // size because preserving the bitmap's aspect ratio implies having to resize based on
+    // whatever dimension requires more aggressive downscaling.
+    while (height / (2 * inSampleSize) > 0.75 * availableHeight ||
+            width / (2 * inSampleSize) > 0.75 * availableWidth) {
+        inSampleSize *= 2
+    }
+    return inSampleSize
+}
+// [1]: https://developer.android.com/topic/performance/graphics/load-bitmap
