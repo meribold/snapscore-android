927f0ab97ff9105cee4f63530972ca2290e5bc7e
Lukas Waymann <meribold@gmail.com>
2018-09-27 11:24:59 +0800

    Connect to my laptop
    Try to connect to the SnapScore server running on my laptop.  In case of
    success, the `TextView` shows "42".

--- app/src/main/java/NetworkTask.kt
+++ app/src/main/java/NetworkTask.kt
@@ -4,6 +4,9 @@ import android.app.Activity
 import android.os.AsyncTask
 import java.io.File
 import java.lang.ref.WeakReference
+import java.io.IOException
+import java.net.Socket
+import java.net.UnknownHostException
 import kotlinx.android.synthetic.main.activity_main.*
 
 class NetworkTask(val actRef: WeakReference<Activity>) : AsyncTask<File, Unit, Int>() {
@@ -12,7 +15,18 @@ class NetworkTask(val actRef: WeakReference<Activity>) : AsyncTask<File, Unit, I
 
     // This runs in the background thread.
     override fun doInBackground(vararg image: File): Int? {
-        Thread.sleep(5000)
+        // Create a TCP socket and connect.
+        try {
+            val socket = Socket("192.168.168.75", 50007)
+        } catch (e: UnknownHostException) {
+            return -1
+        } catch (e: IOException) {
+            return -2
+        } catch (e: SecurityException) {
+            return -3
+        } catch (e: IllegalArgumentException) {
+            return -4
+        }
         return 42
     }
 
